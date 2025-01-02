c76ce06fed4540e7fc0d33a60f92010f53a74c7c
wenjin <hewenjin94@outlook.com>
2025-01-02 11:56:23 +0800

    feat(fcitx5): add dict data pkgs & remove user config

--- modules/home-manager/fcitx5.nix
+++ modules/home-manager/fcitx5.nix
@@ -18,9 +18,20 @@
   i18n.inputMethod = {
     enabled = "fcitx5";
     fcitx5.addons = with pkgs; [
-      fcitx5-rime
+      (fcitx5-rime.override {
+        rimeDataPkgs = [
+          nur.repos.xddxdd.rime-aurora-pinyin
+          nur.repos.xddxdd.rime-custom-pinyin-dictionary
+          nur.repos.xddxdd.rime-dict
+          nur.repos.xddxdd.rime-ice
+          nur.repos.xddxdd.rime-moegirl
+          nur.repos.xddxdd.rime-zhwiki
+          rime-data
+        ];
+      })
       fcitx5-mozc
       fcitx5-chinese-addons
+      fcitx5-gtk
     ];
   };
 }
