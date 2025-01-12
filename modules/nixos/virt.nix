4d4af100fa9c68224608193e28a5d427f8856672
wenjin <hewenjin94@outlook.com>
2025-01-12 22:26:41 +0800

    feat(nix): update flake.lock and reopen some settings that had some compile errors before

--- modules/nixos/virt.nix
+++ modules/nixos/virt.nix
@@ -7,9 +7,9 @@
   ...
 }: {
   # temporary disabled for compile error at this time
-  # environment.systemPackages = with pkgs; [
-  #   quickemu
-  # ];
+  environment.systemPackages = with pkgs; [
+    quickemu
+  ];
   # virtualisation
   programs.virt-manager.enable = true;
   virtualisation.libvirtd.enable = true;
