9b9b7808c02a04a6c9a2c9523e2c709c56f7257b
wenjin <hewenjin94@outlook.com>
2025-01-21 21:21:48 +0800

    chore(nix): update aliyun ssh authorizedkey

--- nixos/hosts/aliyun/configuration.nix
+++ nixos/hosts/aliyun/configuration.nix
@@ -100,6 +100,7 @@
   users.users.root.openssh.authorizedKeys.keys = [
     # change this to your ssh key
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOpMRzF5l2trX2SwRxYn4KcSahIKVbU+T+Li+IE5qMIw wenjin@nixos-wsl"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEtBUbTuGD34mJCUZp7hIFuDR9kACg4y8iWhjAPB5R66 wenjin@nixos"
   ];
 
   system.stateVersion = "24.05";
