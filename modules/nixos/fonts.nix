9df3186cd631fe022e6df7830cab3bea16838ca0
wenjin <hewenjin94@outlook.com>
2025-02-01 11:31:20 +0800

    feat(nix): add more CJK font

--- modules/nixos/fonts.nix
+++ modules/nixos/fonts.nix
@@ -14,9 +14,19 @@
       nerd-fonts.ubuntu
       nerd-fonts.ubuntu-mono
       nerd-fonts.caskaydia-cove
+      source-han-serif-vf-ttf
+      source-han-serif-vf-otf
+      source-han-serif
+      source-han-sans-vf-ttf
+      source-han-sans-vf-otf
+      source-han-sans
+      source-han-mono
+      source-han-code-jp
       font-awesome
       lexend
       material-symbols
+      wqy_zenhei
+      wqy_microhei
       # microsoft fonts
       corefonts
       vistafonts-cht
