7a87c39bb9c9b68365bb5b2173f9f208230fd2ff
wenjin <hewenjin94@outlook.com>
2025-03-10 15:43:43 +0800

    chore(nix): add nixpkgs-review treefmt

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -148,6 +148,8 @@ in {
     # it provides the command `nom` works just like `nix`
     # with more details log output
     nix-output-monitor
+    nixpkgs-review
+    treefmt
   ];
 
   # xresources.properties = {
