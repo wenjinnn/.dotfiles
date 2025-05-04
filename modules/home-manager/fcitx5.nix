64e3276dec6780a97ecb78600de513f69ab7a7e6
wenjin <hewenjin94@outlook.com>
2025-05-04 18:36:26 +0800

    refactor(nix): update flake.lock and adapt breaking changes of it
    use nix to configure fcitx5
    apapt zsh initContent option

--- modules/home-manager/fcitx5.nix
+++ modules/home-manager/fcitx5.nix
@@ -6,16 +6,16 @@
   pkgs,
   ...
 }: {
-  xdg.configFile = {
-    "fcitx5/profile" = {
-      source = ../../xdg/config/fcitx5/profile;
-      # every time fcitx5 switch input method, it will modify ~/.config/fcitx5/profile,
-      # so we need to force replace it in every rebuild to avoid file conflict.
-      force = true;
-    };
-    "fcitx5/config".source = ../../xdg/config/fcitx5/config;
-    "fcitx5/conf/classicui.conf".source = ../../xdg/config/fcitx5/conf/classicui.conf;
-  };
+  # xdg.configFile = {
+  #   "fcitx5/profile" = {
+  #     source = ../../xdg/config/fcitx5/profile;
+  #     # every time fcitx5 switch input method, it will modify ~/.config/fcitx5/profile,
+  #     # so we need to force replace it in every rebuild to avoid file conflict.
+  #     force = true;
+  #   };
+  #   "fcitx5/config".source = ../../xdg/config/fcitx5/config;
+  #   "fcitx5/conf/classicui.conf".source = ../../xdg/config/fcitx5/conf/classicui.conf;
+  # };
   # rime sync dir link to private repo
   xdg.dataFile = {
     "fcitx5/rime/sync" = {
@@ -24,27 +24,75 @@
         "${config.home.sessionVariables.ARCHIVE}/rime/";
     };
   };
-  home.sessionVariables = {
-    # fix fcitx5 gtk module dark theme not work, can be remove after this PR merged: https://github.com/nix-community/home-manager/pull/5431
-    GTK_IM_MODULE = lib.mkForce "";
-  };
   # fcitx5
   i18n.inputMethod = {
-    enabled = "fcitx5";
-    fcitx5.addons = with pkgs; [
-      (fcitx5-rime.override {
-        rimeDataPkgs = [
-          nur.repos.xddxdd.rime-aurora-pinyin
-          nur.repos.xddxdd.rime-custom-pinyin-dictionary
-          nur.repos.xddxdd.rime-dict
-          nur.repos.xddxdd.rime-ice
-          nur.repos.xddxdd.rime-moegirl
-          nur.repos.xddxdd.rime-zhwiki
-          rime-data
-        ];
-      })
-      fcitx5-mozc
-      fcitx5-gtk
-    ];
+    type = "fcitx5";
+    enable = true;
+    fcitx5 = {
+      waylandFrontend = true;
+      addons = with pkgs; [
+        (fcitx5-rime.override {
+          rimeDataPkgs = [
+            nur.repos.xddxdd.rime-aurora-pinyin
+            nur.repos.xddxdd.rime-custom-pinyin-dictionary
+            nur.repos.xddxdd.rime-dict
+            nur.repos.xddxdd.rime-ice
+            nur.repos.xddxdd.rime-moegirl
+            nur.repos.xddxdd.rime-zhwiki
+            rime-data
+          ];
+        })
+        fcitx5-mozc
+        fcitx5-gtk
+      ];
+      settings = {
+        inputMethod = {
+          GroupOrder."0" = "Default";
+          "Groups/0" = {
+            Name = "Default";
+            "Default Layout" = "us";
+            DefaultIM = "keyboard-us";
+          };
+          "Groups/0/Items/0".Name = "keyboard-us";
+          "Groups/0/Items/1".Name = "rime";
+          "Groups/0/Items/2".Name = "mozc";
+        };
+        globalOptions = {
+          Behavior = {
+            ActiveByDefault = false;
+          };
+          Hotkey = {
+            EnumerateWithTriggerKeys = true;
+            EnumerateSkipFirst = false;
+          };
+          "Hotkey/TriggerKeys" = {
+            "0" = "Super+space";
+          };
+          "Hotkey/AltTriggerKeys" = {
+            "0" = "Shift+L";
+          };
+          "Hotkey/EnumerateGroupForwardKeys" = {
+            "0" = "Super+space";
+          };
+          "Hotkey/EnumerateGroupBackwardKeys" = {
+            "0" = "Shift+Super+space";
+          };
+          Behavior = {
+            PreeditEnabledByDefault = true;
+            ShowInputMethodInformation = true;
+            showInputMethodInformationWhenFocusIn = false;
+            CompactInputMethodInformation = true;
+            ShowFirstInputMethodInformation = true;
+          };
+        };
+        addons = {
+          classicui.globalSection = {
+            Theme = "stylix";
+            UseDarkTheme = true;
+          };
+        };
+      };
+
+    };
   };
 }
--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -46,20 +46,22 @@
       n = "ncmpcpp";
       t = "trans";
     };
-    initExtra = let
-      proxyPort = "7890";
-      proxyAddr = "http://127.0.0.1:${proxyPort}";
-    in ''
-      bindkey '^[[D' wd_browse_widget
-      bindkey '^b' backward-char
-      PROXY_ENV=(http_proxy ftp_proxy https_proxy all_proxy HTTP_PROXY HTTPS_PROXY FTP_PROXY ALL_PROXY)
-      NO_PROXY_ENV=(no_proxy NO_PROXY)
-      proxy_addr=${proxyAddr}
-      no_proxy_addr=localhost,127.0.0.1,localaddress,.localdomain.com,10.96.0.0/12,192.168.99.0/24,192.168.39.0/24,192.168.49.2/24
-      if [ $(hostname) = "nixos-wsl" ]; then
-        # get wsl host ip
-        proxy_addr=http://$(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):${proxyPort}
-      fi
+    initContent =
+      let
+        proxyPort = "7890";
+        proxyAddr = "http://127.0.0.1:${proxyPort}";
+      in
+      ''
+        bindkey '^[[D' wd_browse_widget
+        bindkey '^b' backward-char
+        PROXY_ENV=(http_proxy ftp_proxy https_proxy all_proxy HTTP_PROXY HTTPS_PROXY FTP_PROXY ALL_PROXY)
+        NO_PROXY_ENV=(no_proxy NO_PROXY)
+        proxy_addr=${proxyAddr}
+        no_proxy_addr=localhost,127.0.0.1,localaddress,.localdomain.com,10.96.0.0/12,192.168.99.0/24,192.168.39.0/24,192.168.49.2/24
+        if [ $(hostname) = "nixos-wsl" ]; then
+          # get wsl host ip
+          proxy_addr=http://$(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):${proxyPort}
+        fi
 
       proxyIsSet(){
           for envar in $PROXY_ENV
