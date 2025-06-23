3342ef3cb929870699c133a760b95172d6a565bb
wenjin <hewenjin94@outlook.com>
2025-06-23 22:43:55 +0800

    chore(fuzzel): compatible with stylix

--- modules/home-manager/fuzzel.nix
+++ modules/home-manager/fuzzel.nix
@@ -10,19 +10,8 @@
     enable = true;
     settings = {
       main = {
-        font = "DejaVuSansM Nerd Font Mono:size=12";
-        dpi-aware = "yes";
-        terminal = "${pkgs.foot}/bin/footclient -e";
+        terminal = "${pkgs.foot}/bin/foot -e";
         layer = "overlay";
-      };
-      colors = {
-        background = "171717ff";
-        text = "eeeeeeff";
-        selection = "373737ff";
-        selection-text = "c4c4c4ff";
-        border = "1f1f1fff";
-        match = "5ba9e8ff";
-        selection-match = "5ba9e8ff";
       };
       dmenu = {
         exit-immediately-if-empty = "yes";
