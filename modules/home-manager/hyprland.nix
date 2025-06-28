602469b6e4e0f3c4f5d8b37c4fbb738f1644c3f0
wenjin <hewenjin94@outlook.com>
2025-06-28 21:22:11 +0800

    chore(hyprland): bring back customize animation

--- modules/home-manager/hyprland.nix
+++ modules/home-manager/hyprland.nix
@@ -339,6 +339,14 @@ in
           };
           animations = {
             enabled = true;
+            bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
+            animation = [
+              "windows, 1, 5, myBezier"
+              "windowsOut, 1, 7, default, popin 80%"
+              "border, 1, 10, default"
+              "fade, 1, 7, default"
+              "workspaces, 1, 6, default"
+            ];
           };
           xwayland = {
             force_zero_scaling = true;
