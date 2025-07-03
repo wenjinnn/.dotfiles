4598171205d6eab1673d1049a8238a1ac228a78b
wenjin <hewenjin94@outlook.com>
2025-07-03 22:36:00 +0800

    chore(niri): adjust volume step

--- modules/home-manager/niri.nix
+++ modules/home-manager/niri.nix
@@ -170,11 +170,11 @@ in
 
             "XF86AudioRaiseVolume" = {
               allow-when-locked = true;
-              action = sh "wpctl set-volume @DEFAULT_AUDIO_SINK@ 0.1+";
+              action = sh "wpctl set-volume @DEFAULT_AUDIO_SINK@ 0.05+";
             };
             "XF86AudioLowerVolume" = {
               allow-when-locked = true;
-              action = sh "wpctl set-volume @DEFAULT_AUDIO_SINK@ 0.1-";
+              action = sh "wpctl set-volume @DEFAULT_AUDIO_SINK@ 0.05-";
             };
             "XF86AudioMute" = {
               allow-when-locked = true;
