ef8f17e9cbd999f521659b7d828921aeedbb8c72
wenjin <hewenjin94@outlook.com>
2025-07-03 14:40:19 +0800

    fix(yazi): mgr related configuration error

--- modules/home-manager/yazi.nix
+++ modules/home-manager/yazi.nix
@@ -7,12 +7,14 @@
         max_width = 1500;
         max_height = 1500;
       };
-    };
-    theme = {
       mgr = {
         show_hidden = true;
         show_symlink = true;
         linemode = "mtime";
+      };
+    };
+    theme = {
+      mgr = {
         border_symbol = " ";
       };
       status = {
