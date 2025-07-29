725d4e8649cf1ca72cb3535436ebc2abade985ef
wenjin <hewenjin94@outlook.com>
2025-07-29 22:55:24 +0800

    chore(tlp): optimize bat usage

--- nixos/hosts/nixos/configuration.nix
+++ nixos/hosts/nixos/configuration.nix
@@ -7,7 +7,8 @@
   config,
   pkgs,
   ...
-}: {
+}:
+{
   # You can import other NixOS modules here
   imports = with outputs.nixosModules; [
     # If you want to use modules your own flake exports (from modules/nixos):
@@ -74,16 +75,20 @@
         START_CHARGE_THRESH_BAT0 = 75;
         STOP_CHARGE_THRESH_BAT0 = 80;
         PLATFORM_PROFILE_ON_AC = "performance";
-        PLATFORM_PROFILE_ON_BAT = "balanced";
+        PLATFORM_PROFILE_ON_BAT = "low-power";
 
         CPU_SCALING_GOVERNOR_ON_AC = "performance";
         CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
         CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-        CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_performance";
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
 
         CPU_BOOST_ON_AC = 1;
         CPU_BOOST_ON_BAT = 0;
+        CPU_HWP_DYN_BOOST_ON_AC = 1;
+        CPU_HWP_DYN_BOOST_ON_BAT = 0;
+        CPU_MAX_PERF_ON_AC = 100;
+        CPU_MAX_PERF_ON_BAT = 50;
       };
     };
   };
