d48b90f62a69aa229b925fcb4fca0c4c1917adfe
wenjin <hewenjin94@outlook.com>
2025-08-14 21:41:53 +0800

    feat(mpd): add visualizer fifo for ncmpcpp

--- modules/home-manager/mpd.nix
+++ modules/home-manager/mpd.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: {
+{ pkgs, config, ... }:
+let
+  mpd_fifo = "${config.home.homeDirectory}/.local/share/mpd/mpd.fifo";
+in
+{
   home.packages = [
     pkgs.mpc
   ];
@@ -8,14 +12,28 @@
       enable = true;
       extraConfig = ''
         audio_output {
-          type  "pipewire"
-          name  "PipeWire Sound Server"
+          type "pipewire"
+          name "PipeWire Sound Server"
+        }
+        audio_output {
+            type "fifo"
+            name "visualizer"
+            path "${mpd_fifo}"
+            format "44100:16:2"
         }
       '';
     };
   };
   programs.ncmpcpp = {
     enable = true;
+    package = pkgs.ncmpcpp.override { visualizerSupport = true; };
+    settings = {
+      visualizer_data_source = "${mpd_fifo}";
+      visualizer_output_name = "visualizer";
+      visualizer_in_stereo = "yes";
+      visualizer_type = "spectrum";
+      visualizer_look = "+|";
+    };
     bindings = [
       {
         key = "j";
@@ -27,11 +45,17 @@
       }
       {
         key = "J";
-        command = ["select_item" "scroll_down"];
+        command = [
+          "select_item"
+          "scroll_down"
+        ];
       }
       {
         key = "K";
-        command = ["select_item" "scroll_up"];
+        command = [
+          "select_item"
+          "scroll_up"
+        ];
       }
     ];
   };
