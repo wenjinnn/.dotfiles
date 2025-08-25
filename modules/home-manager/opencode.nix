bf6548c7a6ca8332ca6b61533a95cba29c2d9f7c
wenjin <hewenjin94@outlook.com>
2025-08-25 20:39:14 +0800

    feat(opencode): add neovim mcp to opencode

--- modules/home-manager/opencode.nix
+++ modules/home-manager/opencode.nix
@@ -5,13 +5,26 @@
       theme = "gruvbox";
       mcp = {
         context7 = {
+          enabled = true;
           type = "local";
           command = [
             "npx"
             "-y"
             "@upstash/context7-mcp"
           ];
+        };
+        neovim = {
           enabled = true;
+          type = "local";
+          command = [
+            "npx"
+            "-y"
+            "mcp-neovim-server"
+          ];
+          environment = {
+            ALLOW_SHELL_COMMANDS = true;
+            NVIM_SOCKET_PATH = "/tmp/nvim";
+          };
         };
       };
     };
