ac2dae8b8dc00c8bacc4f783cd8d78b46db4f665
wenjin <hewenjin94@outlook.com>
2025-08-26 00:18:11 +0800

    chore(foot): adjust settings

--- modules/home-manager/foot.nix
+++ modules/home-manager/foot.nix
@@ -5,21 +5,11 @@
     enable = true;
     server.enable = true;
     settings = {
+      main = {
+        selection-target = "both";
+      };
       scrollback = {
         lines = 10000;
-      };
-      key-bindings = {
-        scrollback-up-page = "Control+Shift+Page_Up";
-        scrollback-down-page = "Control+Shift+Page_Down";
-        clipboard-copy = "Control+Shift+c";
-        clipboard-paste = "Control+Shift+v";
-        search-start = "Control+Shift+f";
-      };
-      search-bindings = {
-        cancel = "Escape";
-        commit = "Return";
-        find-prev = "Control+Shift+p";
-        find-next = "Control+Shift+n";
       };
     };
   };
