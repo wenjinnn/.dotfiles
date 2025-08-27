6657494182b3243ab460877c910b9d2689ccb88c
wenjin <hewenjin94@outlook.com>
2025-08-27 11:47:33 +0800

    feat(opencode): make NVIM_SOCKET_PATH refresh on each launch

--- modules/home-manager/opencode.nix
+++ modules/home-manager/opencode.nix
@@ -1,31 +1,42 @@
 {
-  home.sessionVariables = {
-    # Get the newest nvim socket to use it with opencode neovim mcp server
-    NVIM_SOCKET_PATH = "$(find \"\${XDG_RUNTIME_DIR:-\${TMPDIR}nvim.\${USER}}/\" -type s -name 'nvim.*.0' -printf '%T@ %p\n' 2>/dev/null | sort -nr | head -n 1 | awk '{print $2}')";
-  };
 
-  programs.opencode = {
-    enable = true;
-    settings = {
-      theme = "gruvbox";
-      mcp = {
-        context7 = {
-          enabled = true;
-          type = "local";
-          command = [
-            "npx"
-            "-y"
-            "@upstash/context7-mcp"
-          ];
-        };
-        neovim = {
-          enabled = true;
-          type = "local";
-          command = [
-            "npx"
-            "-y"
-            "mcp-neovim-server"
-          ];
+  programs = {
+    bash = {
+      shellAliases = {
+        # Alias to connect to the most recent nvim instance
+        opencode = ''
+          env NVIM_SOCKET_PATH=$( \
+                  find "''${XDG_RUNTIME_DIR:-''${TMPDIR}nvim.''${USER}}/" -type s -name 'nvim.*.0' -printf '%T@ %p\n' 2>/dev/null \
+                  | sort -nr \
+                  | head -n 1 \
+                  | awk '{print $2}' \
+                  ) opencode
+        '';
+      };
+    };
+    opencode = {
+      enable = true;
+      settings = {
+        theme = "gruvbox";
+        mcp = {
+          context7 = {
+            enabled = true;
+            type = "local";
+            command = [
+              "npx"
+              "-y"
+              "@upstash/context7-mcp"
+            ];
+          };
+          neovim = {
+            enabled = true;
+            type = "local";
+            command = [
+              "npx"
+              "-y"
+              "mcp-neovim-server"
+            ];
+          };
         };
       };
     };
