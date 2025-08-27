c65f217ec94c4ad9aa72e0e4c089b0a3bc714ed1
wenjin <hewenjin94@outlook.com>
2025-08-27 11:26:40 +0800

    feat(opencode): let neovim mcp server connect to newest neovim instance by default

--- modules/home-manager/opencode.nix
+++ modules/home-manager/opencode.nix
@@ -1,4 +1,9 @@
 {
+  home.sessionVariables = {
+    # Get the newest nvim socket to use it with opencode neovim mcp server
+    NVIM_SOCKET_PATH = "$(find \"\${XDG_RUNTIME_DIR:-\${TMPDIR}nvim.\${USER}}/\" -type s -name 'nvim.*.0' -printf '%T@ %p\n' 2>/dev/null | sort -nr | head -n 1 | awk '{print $2}')";
+  };
+
   programs.opencode = {
     enable = true;
     settings = {
