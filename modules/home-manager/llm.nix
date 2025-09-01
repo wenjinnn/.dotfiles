4830f4624e88cd467cefbb5e39bc3f56b777e2a7
wenjin <hewenjin94@outlook.com>
2025-09-01 21:06:33 +0800

    chore(llm): export current nvim socket path to cli by default

--- modules/home-manager/llm.nix
+++ modules/home-manager/llm.nix
@@ -5,21 +5,21 @@
       shellAliases = {
         # Alias to connect to the most recent nvim instance
         opencode = ''
-          env NVIM_SOCKET_PATH=$( \
+          env NVIM_SOCKET_PATH=''${NVIM:-$( \
                   find "''${XDG_RUNTIME_DIR:-''${TMPDIR}nvim.''${USER}}/" -type s -name 'nvim.*.0' -printf '%T@ %p\n' 2>/dev/null \
                   | sort -nr \
                   | head -n 1 \
-                  | awk '{print $2}' \
+                  | awk '{print $2}')} \
                   ) opencode
         '';
         gemini = ''
           env GEMINI_API_KEY=$(sops exec-env $SOPS_SECRETS 'echo -n $GEMINI_API_KEY') \
           GOOGLE_CLOUD_PROJECT=$(sops exec-env $SOPS_SECRETS 'echo -n $GOOGLE_CLOUD_PROJECT') \
-          NVIM_SOCKET_PATH=$( \
+          NVIM_SOCKET_PATH=''${NVIM:-$( \
                   find "''${XDG_RUNTIME_DIR:-''${TMPDIR}nvim.''${USER}}/" -type s -name 'nvim.*.0' -printf '%T@ %p\n' 2>/dev/null \
                   | sort -nr \
                   | head -n 1 \
-                  | awk '{print $2}') \
+                  | awk '{print $2}')} \
           gemini
         '';
       };
