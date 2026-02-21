ba3b03258cc3065ce63238047e1d67c5491bc0e3
wenjin <hewenjin94@outlook.com>
2026-02-21 10:39:59 +0800

    chore(nix): use ollama default data dir

--- modules/nixos/ollama.nix
+++ modules/nixos/ollama.nix
@@ -9,29 +9,6 @@
   services = {
     ollama = {
       enable = true;
-      home = "/home/ollama";
-      user = "ollama";
-      group = "ollama";
     };
   };
-  users.users = {
-    ollama = {
-      home = "/home/ollama";
-      isSystemUser = true;
-      createHome = true;
-    };
-  };
-
-  # changing ollama home throws error. Follows can fix it, see https://github.com/NixOS/nixpkgs/issues/357604
-  systemd.services.ollama.serviceConfig =
-    let
-      cfg = config.services.ollama;
-      ollamaPackage = cfg.package;
-    in
-    lib.mkForce {
-      Type = "exec";
-      ExecStart = "${lib.getExe ollamaPackage} serve";
-      WorkingDirectory = cfg.home;
-      SupplementaryGroups = ["render"];
-    };
 }
