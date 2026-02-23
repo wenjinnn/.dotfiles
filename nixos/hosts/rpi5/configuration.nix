4823e8a06f7b503431ff30fcfa7e007b81cd00cb
wenjin <hewenjin94@outlook.com>
2026-02-23 20:17:40 +0800

    chore(nix): adjust rpi5 aria2 settings

--- nixos/hosts/rpi5/configuration.nix
+++ nixos/hosts/rpi5/configuration.nix
@@ -167,6 +167,7 @@
       downloadDirPermission = "2777";
       settings = {
         config-path = "/mnt/data/video/aria2/aria2.conf";
+        save-session = "/mnt/data/video/aria2/aria2.session";
         bt-tracker = "udp://tracker.opentrackr.org:1337/announce,udp://open.demonoid.ch:6969/announce,udp://open.demonii.com:1337/announce,udp://open.stealth.si:80/announce,udp://tracker.torrent.eu.org:451/announce,udp://explodie.org:6969/announce,udp://tracker1.myporn.club:9337/announce,udp://tracker.theoks.net:6969/announce,udp://tracker.qu.ax:6969/announce,udp://tracker.filemail.com:6969/announce,udp://tracker.dler.org:6969/announce,udp://tracker.bittor.pw:1337/announce,udp://tracker.0x7c0.com:6969/announce,udp://tracker-udp.gbitt.info:80/announce,udp://run.publictracker.xyz:6969/announce,udp://retracker01-msk-virt.corbina.net:80/announce,udp://p4p.arenabg.com:1337/announce,udp://opentracker.io:6969/announce,udp://open.tracker.cl:1337/announce,udp://leet-tracker.moe:1337/announce";
         dir = "/mnt/data/video/aria2";
         enable-rpc = true;
