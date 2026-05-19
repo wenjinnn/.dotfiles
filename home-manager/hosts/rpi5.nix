a0e51ee862ab85a5f87da48cdea5c8b8f527dd70
wenjin <hewenjin94@outlook.com>
2026-05-19 21:37:37 +0800

    fix(nix): rpi5 home config not work

--- home-manager/hosts/rpi5.nix
+++ home-manager/hosts/rpi5.nix
@@ -16,9 +16,27 @@
     sops
     startship
   ];
+  nixpkgs = {
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+    };
+  };
+
   home = {
     username = "${me.username}";
     homeDirectory = "/home/${me.username}";
   };
+  home.sessionPath = [
+    # add customize script path
+    "${config.home.homeDirectory}/.local/bin"
+  ];
+  home.sessionVariables = {
+    # sops secrets file location
+    SOPS_SECRETS = "${config.home.homeDirectory}/.dotfiles/secrets.yaml";
+  };
+
+  home.stateVersion = "26.05";
   programs.bash.enable = true;
 }
