34f70e8d8f2643a663eba6ca9ec4c798fa839030
wenjin <hewenjin94@outlook.com>
2026-05-30 14:35:18 +0800

    chore(nix): enable tlp pd

--- nixos/hosts/nixos/configuration.nix
+++ nixos/hosts/nixos/configuration.nix
@@ -70,6 +70,7 @@
     power-profiles-daemon.enable = lib.mkForce false;
     tlp = {
       enable = true;
+      pd.enable = true;
       settings = {
         START_CHARGE_THRESH_BAT0 = 75;
         STOP_CHARGE_THRESH_BAT0 = 80;
