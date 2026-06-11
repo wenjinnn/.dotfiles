d50cb2d12c2a3bf08b735f8c67a5ae6c0df81bcb
wenjin <hewenjin94@outlook.com>
2026-06-11 21:18:30 +0800

    chore(nix): disable tlp turbo control

--- nixos/hosts/nixos/configuration.nix
+++ nixos/hosts/nixos/configuration.nix
@@ -84,9 +84,9 @@
         CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
         CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
 
-        CPU_BOOST_ON_AC = 1;
+        CPU_BOOST_ON_AC = -1;
         CPU_BOOST_ON_BAT = 0;
-        CPU_HWP_DYN_BOOST_ON_AC = 1;
+        CPU_HWP_DYN_BOOST_ON_AC = -1;
         CPU_HWP_DYN_BOOST_ON_BAT = 0;
         CPU_MAX_PERF_ON_AC = 100;
         CPU_MAX_PERF_ON_BAT = 70;
