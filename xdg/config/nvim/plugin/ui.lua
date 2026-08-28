7f5dfbbb1d6479a8068ce1a4e8f6fce97f764653
wenjin <hewenjin94@outlook.com>
2026-08-28 09:29:26 +0800

    feat(neovim): update nvim-pack-lock and enable mini.statuscolumn

--- xdg/config/nvim/plugin/ui.lua
+++ xdg/config/nvim/plugin/ui.lua
@@ -28,6 +28,7 @@ starter.setup({
 })
 
 later(function()
+  require('mini.statuscolumn').setup()
   require('mini.indentscope').setup({
     draw = {
       animation = require('mini.indentscope').gen_animation.none(),
